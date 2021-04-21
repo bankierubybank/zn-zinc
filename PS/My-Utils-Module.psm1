597a7da1eb7e7f8a6a56f74b22171cde51150689
Chatchai Wongdetsakul <chatchai.wongdet@gmail.com>
2021-04-21 20:46:41 +0700

    First Commit

---
+++ PS/My-Utils-Module.psm1
@@ -0,0 +1,61 @@
+﻿New-Module -Name My-Utils-Module -ScriptBlock {
+
+    function Get-COMPorts {
+        <#
+        .SYNOPSIS
+        Return a list of COM ports connected to the specified computer.
+
+        .DESCRIPTION
+        Get-COMPorts is a function that returns a list of COM ports connected to the specified computer.
+
+        .EXAMPLE
+        Get-COMPorts
+
+        .INPUTS
+        NONE
+        #>
+        
+        (Get-CimInstance -query "SELECT * FROM Win32_PnPEntity" | Where-Object { $_.Name -Match "COM\d+" }) | Select-Object -Property Name, Status, Present
+    }
+
+    function Get-WiFiHealth {
+        <#
+        .SYNOPSIS
+        Return an information of WLAN connected.
+
+        .DESCRIPTION
+        Get-WiFiHealth is a wrapper of "netsh wlan show interfaces" command's output and return as PSCustomObject.
+
+        .EXAMPLE
+        Get-WiFiHealth
+
+        .INPUTS
+        NONE
+
+        .OUTPUTS
+        PSCustomObject
+
+        .NOTES
+        Author:  Chatchai Wongdetsakul
+        #>
+
+        #(netsh wlan show interfaces) -Match '^\s+Signal' -Replace '^\s+Signal\s+:\s+',''
+
+        $output = (netsh wlan show interfaces) -Match '\s{2,}:\s'
+        $wifiObj = [PSCustomObject]@{
+            Name = ($output | Select-String -Pattern 'Name' -Raw).Split(":").Trim()[-1]
+            Description = ($output | Select-String -Pattern 'Description' -Raw).Split(":").Trim()[-1]
+            SSID = (($output | Select-String -Pattern 'SSID' -Raw) | Select-Object -First 1).Split(":").Trim()[-1]
+            Radio = ($output | Select-String -Pattern 'Radio Type' -Raw).Split(":").Trim()[-1]
+            Authentication = ($output | Select-String -Pattern 'Authentication' -Raw).Split(":").Trim()[-1]
+            Cipher = ($output | Select-String -Pattern 'Cipher' -Raw).Split(":").Trim()[-1]
+            Channel = ($output | Select-String -Pattern 'Channel' -Raw).Split(":").Trim()[-1]
+            ReceiveRateMbps = ($output | Select-String -Pattern 'Receive rate' -Raw).Split(":").Trim()[-1]
+            TransmitRateMbps = ($output | Select-String -Pattern 'Transmit rate' -Raw).Split(":").Trim()[-1]
+            Signal = ($output | Select-String -Pattern 'Signal' -Raw).Split(":").Trim()[-1]
+        }
+        $wifiObj
+    }
+
+    Export-ModuleMember -Function Get-COMPorts, Get-WiFiHealth
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# My PS/SH Scripts
+
+### Just to learn how to write PS/SH scripts
+
+### Deployment
+* Any PC with PowerShell installed
+* Any PC with Linux shell (bash) installed
+
+### PS Cheat Sheet   
+```sh
+$ Import-Module .\PS\My-Utils-Module.psm1
+$ Remove-Module My-Utils-Module
+```
